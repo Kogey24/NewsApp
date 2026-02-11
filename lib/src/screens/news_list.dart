14e006fad3d484013507fd64a653d0928ec9fcdc
Harmmerton Kogi <hammertonkogey@gmail.com>
2026-02-11 21:05:51 +0300

    Used the streambuilder to build the list of topIss available in the stream

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -8,13 +8,33 @@ class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = StoriesProvider.of(context);
+
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Top News'),
         backgroundColor: Colors.amberAccent,
       ),
-      body: Center(child: Text("SHOW A LIST")),
+      body: buildList(bloc),
       // body: buildList(),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return ListView.builder(
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, int index) {
+            return ListTile(title: Text('${snapshot.data![index]}'));
+          },
+        );
+      },
     );
   }
 
